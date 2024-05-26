71cbe4e5d4f549bf82768d0717d7d3e6250ac6be
Haris Ibragic <ibragicharis01@gmail.com>
2024-05-26 20:06:18 +0200

    prvi commit

---
+++ src/pages/Error.js
@@ -0,0 +1,35 @@
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+
+// const Error = () => {
+//   return (
+//     <section className='error-page section'>
+//       <div className='error-page section'>
+//         <h1>oops! it's dead end...</h1>
+//         <Link to='/' className='btn btn-primary'>
+//           back home
+//         </Link>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Error;
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Error = () => {
+  return (
+    <section className='error-page section'>
+      <div className='error-page section'>
+        <h1>oops! it's dead end...</h1>
+        <Link to='/' className='btn btn-primary'>
+          Back home
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default Error;
